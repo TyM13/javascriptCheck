66e28cbfc73a8b8b08dbe093df90f24376af31ff
Your Name <you@example.com>
2022-08-09 19:16:43 -0600

    ScriptExample knowledge check

---
+++ src/components/ScriptExample.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+
+        data() {
+            return {
+                tweets: [`tweet1`, `tweet2`, `tweet3`],
+                num_friends: 3,
+                logged_in: true,
+            }
+        },
+
+        name: "script-example"
+    }
+</script>
+
+<style scoped>
+
+</style>
